aa60c4f4be65223ad2b416ad890fa0749ae2bb33
Andrew M Westling & Erica Gordon <student@epicodus.com>
2014-07-30 12:02:49 -0700

    add UI

--- lib/List.rb
+++ lib/List.rb
@@ -2,7 +2,7 @@ class List
   def initialize (list_name)
   @list_name = list_name
   @tasks = []
-  @completed = []
+  @finished = []
   end
 
   def list_name
@@ -13,8 +13,17 @@ class List
     @tasks
   end
 
+  def finished
+    @finished
+  end
+
   def add_task (task_description)
     @tasks << task_description
+  end
+
+  def finish_task (task_number)
+    @finished << @tasks[(task_number)-1]
+    @tasks.slice!((task_number)-1)
   end
 
 end
--- to_do.rb
+++ to_do.rb
@@ -2,12 +2,13 @@ require './lib/Task'
 require './lib/List'
 
 @lists = []
-@current_list = ''
+@current_list = nil
 
 def main_menu
   loop do
     puts "Press 'c' to create a list. Press 's' to select a list."
     puts "Press 'a' to add a task or 'l' to list all of your tasks."
+    puts "Press 'f' to mark a task as finished. Press 'z' to show completed tasks."
     puts "Press 'x' to exit."
     main_choice = gets.chomp
 
@@ -19,6 +20,10 @@ def main_menu
       add_task
     elsif main_choice == 'l'
       list_tasks
+    elsif main_choice == 'f'
+        finish_task
+    elsif main_choice == 'z'
+        show_finished
     elsif main_choice == 'x'
       puts "Good-bye!"
       exit
@@ -29,36 +34,90 @@ def main_menu
 end
 
 def create_list
+  puts "\n\n"
   puts "Please enter the name of your list."
   user_list_name = gets.chomp
   @lists << List.new(user_list_name)
+  puts "\n\n"
   puts "List #{user_list_name} created!"
+  puts "\n\n"
 end
 
 def select_list
+  puts "\n\n"
   puts "Here are your lists:"
   @lists.each_with_index do |list, i|
     puts "#{i+1}. #{list.list_name}"
   end
+  puts "\n\n"
   puts "Type the number of the list you want to select."
-  list_number = gets.chomp
-  @current_list = @lists[(list_number.to_i)-1]
-  puts @current_list
+  @list_number = gets.chomp
+  @current_list = @lists[(@list_number.to_i)-1]
+  puts "\n\n"
 end
 
-# def add_task
-#   puts "Enter a description of the new task:"
-#   user_description = gets.chomp
-#   @list << Task.new(user_description)
-#   puts "Task added.\n\n"
-# end
+def add_task
+  if @current_list == nil
+    puts "\n\n"
+    puts "Please select a list first."
+    puts "\n\n"
+  else
+    puts "\n\n"
+    puts "Enter a description of the new task:"
+    puts "\n\n"
+    user_description = gets.chomp
+    @current_list.tasks << Task.new(user_description)
+    puts "\n\n"
+    puts "Task added.\n\n"
+  end
+end
 
-# def list_tasks
-#   puts "Here are all of your tasks:"
-#   @list.each do |task|
-#     puts task.description
-#   end
-#   puts "\n"
-# end
+def list_tasks
+  if @current_list == nil
+    puts "\n\n"
+    puts "Please select a list first."
+    puts "\n\n"
+  else
+    puts "\n\n"
+    puts "Here are all of your tasks:"
+    @current_list.tasks.each_with_index do |task, i|
+      puts "#{i+1}. #{task.description}"
+    end
+  end
+  puts "\n"
+end
+
+def finish_task
+  if @current_list == nil
+    puts "\n\n"
+    puts "Please select a list first."
+    puts "\n\n"
+  else
+    puts "\n\n"
+    puts "Enter which task number you want to mark finished:"
+    user_task_number = gets.chomp
+    user_task_number.to_i
+    @current_list.finish_task(user_task_number.to_i)
+    puts "Task #{user_task_number} deleted!"
+    puts "\n\n"
+  end
+end
+
+def show_finished
+  if @current_list == nil
+    puts "\n\n"
+    puts "Please select a list first."
+    puts "\n\n"
+  else
+    puts "\n\n"
+    puts "Here are your completed tasks:"
+    @current_list.finished.each_with_index do |task, i|
+      puts "#{i+1}. #{task.description}"
+    end
+    puts "\n\n"
+  end
+end
+
+
 
 main_menu
